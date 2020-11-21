9da0c41628178c17f278522ac12b142bb1a7ced2
Dmitry <agapov.box@gmail.com>
2020-11-21 20:38:34 +0300

    Added ability to create books

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -8,10 +8,21 @@ class Api::V1::BooksController < ApiController
     render json: @book
   end
 
+  def create
+    @book = Book.new(book_params_on_create)
+
+    if @book.save
+      render json: @book, status:	:created
+    else
+      render json: @book.errors, status: :unprocessable_entity
+    end
+  end
+
+
   def update
     @book = Book.find(params[:id])
 
-    if @book.update(book_params)
+    if @book.update(book_params_on_update)
       render json: @book, status: :ok
     else
       render json: @book.errors, status: :unprocessable_entity
@@ -20,7 +31,11 @@ class Api::V1::BooksController < ApiController
 
   private
 
-  def book_params
+  def book_params_on_create
+    params.require(:book).permit(:title, :publisher_id)
+  end
+
+  def book_params_on_update
     params.require(:book).permit(:title)
   end
 
--- app/models/book.rb
+++ app/models/book.rb
@@ -4,5 +4,7 @@ class Book < ApplicationRecord
   belongs_to :publisher
   validates :title,
             presence: true,
+            uniqueness: true,
             length: { minimum: 4, maximum: 40 }
+  validates :publisher_id, presence: true, :on => :create
 end
